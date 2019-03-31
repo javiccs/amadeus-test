39da061535dfb23313d89b891d09ba31755a514a
German Javier Perez Gonzalez <gperez10@gmail.com>
2019-03-31 11:11:53 -0500

    Creating components
    Home, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {StoreModule} from '@ngrx/store';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
