b80d0b5f63e7f1c9cadf224bc5f9cf4c7e5118d2
German Javier Perez Gonzalez <gperez10@gmail.com>
2019-03-31 17:23:45 -0500

    Fix const name

--- src/app/passengers/passengers.module.ts
+++ src/app/passengers/passengers.module.ts
@@ -5,11 +5,14 @@ import { PassengerComponent } from './passenger/passenger.component';
 import { PassengerAddComponent } from './passenger-add/passenger-add.component';
 import { PassengerEditComponent } from './passenger-edit/passenger-edit.component';
 import { PassengerListComponent } from './passenger-list/passenger-list.component';
+import { StoreModule } from "@ngrx/store";
+import { passengerReducer } from "./state/passenger.reducer";
 
-const passagerRoutes: Routes = [{ path: '', component: PassengerComponent }];
+const passengerRoutes: Routes = [{ path: '', component: PassengerComponent }];
 
 @NgModule({
   declarations: [PassengerComponent, PassengerAddComponent, PassengerEditComponent, PassengerListComponent],
-  imports: [CommonModule, RouterModule.forChild(passagerRoutes)],
+  imports: [CommonModule, RouterModule.forChild(passengerRoutes),
+    StoreModule.forFeature("passengers", passengerReducer)],
 })
 export class PassengersModule { }
