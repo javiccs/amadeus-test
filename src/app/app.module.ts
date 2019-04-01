6ae387106b048815f9dd43eaa5a6ec3da2c9c749
German Javier Perez Gonzalez <gperez10@gmail.com>
2019-03-31 22:53:51 -0500

    app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {StoreModule} from '@ngrx/store';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { StoreDevtoolsModule } from "@ngrx/store-devtools"
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { EffectsModule } from '@ngrx/effects';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +19,11 @@ import { StoreDevtoolsModule } from "@ngrx/store-devtools"
     BrowserModule,
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument(),
-    AppRoutingModule
+    EffectsModule.forRoot([]),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
