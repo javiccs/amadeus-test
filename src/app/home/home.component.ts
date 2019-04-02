02890e4557e993cf1309b83e3dab8ec92aa251db
German Javier Perez Gonzalez <gperez10@gmail.com>
2019-04-02 00:20:40 -0500

    Home module integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Passenger} from '../passengers/passenger.model';
+import {Flight} from '../flight/flight.model';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,40 @@ import {Passenger} from '../passengers/passenger.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  passenger: Passenger
-  constructor() { }
+  init: boolean;
+  step: string;
+  order: any = {};
+  @Output() addPassengerChild = new EventEmitter<Passenger>()
+  @Output() addFlightChild = new EventEmitter<Flight>()
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.init = true;
+
   }
-  test(currentPassanger: Passenger) {
-      console.log(currentPassanger);
+
+  addPassenger(currentPassenger: Passenger) {
+    console.log(currentPassenger)
+    this.order.passenger = currentPassenger;
+    this.step = 'flight';
+    console.log(this.order);
   }
+
+  addFlight(currentFlight: Flight) {
+    console.log(currentFlight)
+    this.order.flight = currentFlight
+    this.step = 'order';
+    console.log(this.order);
+  }
+
+
+  continue() {
+    this.init = false;
+    this.step = 'passenger';
+  }
+
 
 }
